c355f00781eb1a080fecfdc1b41e39edd3120ce4
Rust <1997rustem@gmail.com>
2023-11-05 13:04:11 +0300

    Homework for 4th lesson

--- Program.cs
+++ Program.cs
@@ -60,3 +60,53 @@
 // {
 // 	Console.WriteLine("да");
 // }
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.Write("Введите число A: ");
+// int A = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите степень B: ");
+// int B = int.Parse(Console.ReadLine());
+
+// int result = 1;
+
+// for (int i = 0; i < B; i++)
+// {
+//     result *= A;
+// }
+// Console.WriteLine($"{A}^{B} = {result}");
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+// int sum = 0;
+
+// while (number > 0)
+// {
+//     sum += number % 10;
+//     number /= 10;
+// }
+// Console.WriteLine($"Сумма цифр в числе: {sum}");
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] array = new int[8];
+
+//     for (int i = 0; i < 8; i++)
+//     {
+//         Console.Write($"Введите элемент {i + 1}: ");
+//         array[i] = int.Parse(Console.ReadLine());
+//     }
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
